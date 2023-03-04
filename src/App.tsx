920d52abeb9bbd896fbe631dd1d46c57f724c33d
albae69 <ahmadalbaihaqi69@gmail.com>
2023-03-04 08:24:16 +0700

    added tailwindcss, react-hook-form,zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Form from './Form'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='max-w-xl mx-auto w-full'>
+      <div className='flex justify-center my-12'>
+        <div className='w-full lg:w-11/12 bg-white p-5 rounded-lg shadow-xl'>
+          <h3 className='pt-4 text-2xl text-center font-bold'>
+            Create New Account
+          </h3>
+          <Form />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
